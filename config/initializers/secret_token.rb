71f6bee375430794599cac2e98ecb5a9857b657e
lkbgift <lkbgift@gmail.com>
2013-04-18 22:51:53 -0400

    adding devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wecity::Application.config.secret_token = '7dcc2e85e6a7b900e21921d5a65e9f03ed62d6a2de8d5f7b67b5194fe0c4205383be4c574a6a8c00ada797b1631e1476b8b6a65a0a9fe1765cb2351231a6fb97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wecity::Application.config.session_store :cookie_store, key: '_wecity_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wecity::Application.config.session_store :active_record_store
